2f9eba939051830241d81207ce58303ed3816c66
Usman Yusuf <me@usyyusuf55.com>
2023-01-03 13:48:12 +0100

    'ch7'

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'crispy_forms',
+    'allauth',
+    'allauth.account',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -137,4 +141,22 @@ STATICFILES_FINDERS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL ='home'
-LOGOUT_REDIRECT_URL = 'home'
+ACCOUNT_LOGOUT_REDIRECT = 'home'
+
+
+# django-allauth config
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS =(
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_SESSION_REMEMBER = True
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = True
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 # Django admin
     path('admin/', admin.site.urls),
 # User management
-    path('accounts/', include('django.contrib.auth.urls')), # new
+    path('accounts/', include('allauth.urls')), # new
 # Local apps
     path('accounts/', include('users.urls')),
     path('', include('pages.urls')),
