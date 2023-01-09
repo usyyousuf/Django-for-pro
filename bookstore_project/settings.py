376597f3774e452c1e1b465b74074add6b2e205e
Usman Yusuf <me@usyyusuf55.com>
2023-01-09 10:50:22 +0100

    Revert "'ch9'"
    This reverts commit 488238642ab5b0a27dd62ed8d51d49f869bb7f38.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -158,7 +158,7 @@ AUTHENTICATION_BACKENDS =(
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
@@ -166,5 +166,3 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = True
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
-
-DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
